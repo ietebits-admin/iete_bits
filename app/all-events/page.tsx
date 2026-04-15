e139e51eeaf03f59949bb580a333b658b7630689
Rimisha <rimikumari159@gmail.com>
2026-04-16 02:36:08 +0530

    added navbar

--- app/all-events/page.tsx
+++ app/all-events/page.tsx
@@ -1,15 +1,17 @@
 
 "use client";
+// import Navbar from ".//components/Navbar";
 
 import EventHero from "./components/EventHero";
 import YearSection from "./components/YearSection";
 import eventsData from "./data/eventsData";
 import Footer from "./components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function EventPage() {
   return (
     <main className="text-white">
-
+     <Navbar/>
       <EventHero />
 
       {eventsData.map((yearData, index) => (
